0d4b64f79bc07cfd03477643049be4c078b90e13
Feagurth <Feagurth@gmail.com>
2014-10-30 01:39:37 +0100

    Tarea 2
    Finalizada Tarea 2

--- DWES_Tarea_2/index.php
+++ DWES_Tarea_2/index.php
@@ -23,13 +23,63 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
     </head>
     <body>
         <?php
+        // Creamos el array agenda donde se almacenarán el nombre y el teléfono
+        // de cada uno de los registros de la agenda
         $agenda;
         $agenda = array();
+
+        /**
+         * Función para validar un nombre
+         * @param type $nombre Nombre a validar
+         * @return boolean True si es correcto, False si es incorrecto
+         */
+        function validaNombre($nombre) {
+            // Validamos el nombre pasado como parámetro usando expresiones 
+            // regulares
+            if (preg_match("/^[a-zA-ZñÑáÁéÉíÍóÓúÚ ]+$/", $nombre)) {
+                // Si es correcto, devolvemos True
+                return TRUE;
+            } else {
+                // Si no es correcto, devolvemos False
+                return FALSE;
+            }
+        }
+
+        /**
+         * Función que valida un número de telefono
+         * @param type $telefono Numero de teléfono a validar
+         * @return boolean True si el telefono es correcto, False si no es 
+         * correcto
+         */
+        function validarTelefono($telefono) {
+
+            // Comprobamos si el telefono no es un valor vacio
+            if ($telefono != "") {
+
+                // Si no lo es, lo validamos usando expresiones regulares
+                // Situamos \b delante y detrás de la expresión regular
+                // para indicar el limite del patrón de busqueda y que no 
+                // devuelva como validados patrones parciales, como por ejemplo
+                // números de 10 o de menos cifras que si cumplan la regla de 
+                // validación de la expresión regular
+                if (preg_match("/\b^[9|8|6|7][0-9]{8}\b/", $telefono)) {
+                    // Devolvemos True si es válido
+                    return TRUE;
+                } else {
+                    // Devolvemos False si no es válido
+                    return FALSE;
+                }
+            } else {
+                // Devolvemos verdadero si su valor es vacío, para permitir la 
+                // eliminación de registros
+                return TRUE;
+            }
+        }
         ?>
-        <table border="1">
+        <table class="contenedor">
             <tr>
                 <td>
-                    <table class="borde">
+                    <table class="agenda">
                         <thead>
                             <tr>
                                 <th>Nombre</th>
@@ -39,50 +89,114 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
                         <tbody>
 
                             <?php
+                            // Creamos una variable para almacenar los errores 
+                            // y mostrarselos después al usuario
                             $error = "";
 
+                            // Comprobamos si el formulario devuelva valores 
+                            // para la agenda
                             if (!empty($_POST['array'])) {
+                                // Si los datos de la agenda no son vacios, 
+                                // deserializamos los datos y los asignamos al 
+                                // a la variable $agenda
                                 $agenda = unserialize($_POST['array']);
                             }
 
+                            // Comprobamos si el formulario devuelve valores 
+                            // para el nombre
                             if (!empty($_POST['nombre'])) {
+
+                                // Si devuelve datos par el nombre, los 
+                                // almacenamos así como la información que 
+                                // pudiese venir para el telefono, sea esta 
+                                // nula o no
                                 $dato_nombre = $_POST['nombre'];
                                 $dato_tlf = $_POST['tlf'];
 
-                                if (!empty($agenda)) {
+                                // Validamos el nombre y el número de teléfono 
+                                if (validaNombre($dato_nombre) && validarTelefono($dato_tlf)) {
 
-                                    if (!in_array($dato_nombre, $agenda)) {
-                                        if ($dato_tlf != "") {
-                                            $agenda[$dato_nombre] = $dato_tlf;
+                                    // Si el nombre y el número de telefono son 
+                                    // correctos, o nulo en el caso del telefono, 
+                                    // comprobamos si tenemos datos en la variable 
+                                    // $agenda
+                                    if (!empty($agenda)) {
+
+                                        // Comprobamos si el nombre que trae el 
+                                        // formulario no se encuentra ya en el array. 
+                                        // Si no está en el array será una operación 
+                                        // de insercción, en caso contrario será una 
+                                        // operación de actualización o de borrado.
+                                        if (!array_key_exists($dato_nombre, $agenda)) {
+
+                                            // Verificamos si tenemos datos del teléfono
+                                            if ($dato_tlf != "") {
+                                                $agenda[$dato_nombre] = $dato_tlf;
+                                            } else {
+                                                // Si no hay teléfono, mostramos un error
+                                                $error = "Debe introducir un número<br>"
+                                                        . "de teléfono para insertar un<br>"
+                                                        . "nuevo registro";
+                                            }
+                                        } else {
+
+                                            // Comprobamos si tenemos un telefono. 
+                                            // De tenerlo será una insercción, 
+                                            // en caso contrario un borrado
+                                            if ($dato_tlf == "") {
+
+                                                // Como no tenemos un teléfono, 
+                                                // borramos el registro del array
+                                                unset($agenda[$dato_nombre]);
+                                            } else {
+                                                // Como tenemos teléfono, añadimos 
+                                                // el registro al array
+                                                $agenda[$dato_nombre] = $dato_tlf;
+                                            }
                                         }
                                     } else {
-                                        if ($dato_tlf == "") {
-                                            unset($agenda[$dato_nombre]);
-                                        } else {
+                                        // Si la agenda está vacía, solo puede 
+                                        // haber insercciones. Comprobamos el teléfono.
+                                        if ($dato_tlf != "") {
+
+                                            // Si hay teléfono, insertamos el 
+                                            // registro en el array
                                             $agenda[$dato_nombre] = $dato_tlf;
+                                        } else {
+                                            // Si no hay teléfono, mostramos un error
+                                            $error = "Debe introducir un número<br>"
+                                                    . "de teléfono para insertar un<br>"
+                                                    . "nuevo registro";
                                         }
                                     }
                                 } else {
-                                    if ($dato_tlf != "") {
-                                        $agenda[$dato_nombre] = $dato_tlf;
-                                    } else {
-                                        //TODO: Meter mensaje de error
-                                        $error = "Introduzca un teléfono";
-                                    }
+                                    // Si el nombre o el número de teléfono 
+                                    // no son válidos, mostramos un mensaje de 
+                                    // error
+                                    $error = "Introduzca un nombre y<br>"
+                                            . "un número válidos";
                                 }
                             } else {
+                                // Si no se ha introducido un nombre y el array 
+                                // está vacío, mostramos un mensaje de error
                                 if (!empty($_POST['array'])) {
-                                    //TODO: Meter mensaje de error
-                                    $error = "Introduzca un nombre";
+
+                                    $error = "Introduzca un nombre<br>"
+                                            . "para introducir un nuevo<br>"
+                                            . "registro";
                                 }
                             }
 
+                            // Iteramos por el array y construimos tantas filas 
+                            // como registros tengamos, incluyendo en cada una 
+                            // dos columnas, una para el nombre y otra para el 
+                            // teléfono
                             foreach ($agenda as $nombre => $tlf) {
                                 print "<tr>";
-                                print "<td class='borde'>";
+                                print "<td class='contenedor'>";
                                 print $nombre;
                                 print "</td>";
-                                print "<td class='borde'>";
+                                print "<td class='contenedor'>";
                                 print $tlf;
                                 print "</td>";
                                 print "</tr>";
@@ -115,19 +229,25 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
                             </tr>
                             <tr>
                                 <td>
+                                    <!-- Serializamos el array $agenda y lo 
+                                    asignamos a un objeto input oculto en el 
+                                    formulario, para poder enviarlo y mantener 
+                                    los datos almacenados-->
                                     <input type='hidden' name='array' value="<?php echo htmlentities(serialize($agenda)); ?>" />
                                     <input type="submit" value="Enviar" name="enviar"/>
                                 </td>
-                                <td>
-                                    <?php
-                                    if ($error != "") {
-                                        print $error;
-                                    }
-                                    ?>
+                                <?php
+                                // Comprobamos si tenemos algún error almacenado
+                                if ($error != "") {
 
-                                </td>
+                                    // De ser así, crearemos una columna más 
+                                    // para mostrar el error
+                                    print "<td class='error'>";
+                                    print $error;
+                                    print "</td>";
+                                }
+                                ?>
                             </tr>
-
                         </table>                        
                     </form>
                 </td>
